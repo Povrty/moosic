a946fd17b37cee71719e3cd5c10a9d61a4c61ab6
Povrty <iakwan@s.sfusd.edu>
2020-04-21 01:05:06 +0000

    Added iframe using soundcloud API.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $( "#searchbutton" ).click(function() {
     let searchtext = $("#searchbar").val();
-    alert ( "You searched for: " + searchtext );
+
+    fetch(`https://api.soundcloud.com/tracks?q=${searchtext}&client_id=5aa8e389ba4e24b6106af5159ab3e344`)
+    .then(function(response) {  
+      return response.json();
+    })
+    .then(function(data){ 
+        console.log(data[0].id);
+        $("body").append(`<iframe width="50%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${data[0].id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
+        console.log("Hello World!")
+    });  
 });
 
