f28ea91f18ee6f0657af3824868cd47797fb2b27
TayunIxcoy <yonathantayun12@gmail.com>
2020-04-21 01:26:15 +0000

    we added our logo

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ $( "#searchbutton" ).click(function() {
       return response.json();
     })
     .then(function(data){ 
-        console.log(data[0].id);
-        $("body").append(`<iframe width="50%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${data[0].id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
-        console.log("Hello World!")
+        $(".songList").html(`<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${data[0].id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
     });  
 });
 
