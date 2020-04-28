f56e8ed2d2d1e16c90fc4e2b1c8a125f3cf88843
TayunIxcoy <yonathantayun12@gmail.com>
2020-04-28 01:32:48 +0000

    Fix merge confict :)

--- script.js
+++ script.js
@@ -2,11 +2,31 @@ $( "#searchbutton" ).click(function() {
     let searchtext = $("#searchbar").val();
 
     fetch(`https://api.soundcloud.com/tracks?q=${searchtext}&client_id=5aa8e389ba4e24b6106af5159ab3e344`)
-    .then(function(response) {  
+    .then(function(response) {
       return response.json();
     })
     .then(function(data){ 
-        $(".songList").html(`<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${data[0].id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
+            $(".songList").empty();
+            $(".songList").append("<h2>" + "Songs" + "</h2>")
+        data.forEach(function(song){
+            $(".songList").append(`<iframe width="51%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${song.id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
+        })
+    });  
+});
+
+$( "#recommendationSearch").click(function() {
+    let searchtext = $("#searchbar2").val();
+
+    fetch(`https://api.soundcloud.com/tracks?genres=${searchtext}&client_id=5aa8e389ba4e24b6106af5159ab3e344`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data){ 
+            $(".songList").empty();
+            $(".songList").append("<h2>" + "Recommendations" + "</h2>")
+        data.forEach(function(song){
+            $(".songList").append(`<iframe width="51%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${song.id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
+        })
     });  
 });
 
