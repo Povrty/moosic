c9c59712df16817620e50c9c07dd29bd86e365cc
Povrty <iakwan@s.sfusd.edu>
2020-05-05 00:49:54 +0000

    Changed Bootstrap presets.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$(".songList").hide();
 $( "#searchbutton" ).click(function() {
     let searchtext = $("#searchbar").val();
 
@@ -9,10 +10,10 @@ $( "#searchbutton" ).click(function() {
             $(".songList").empty();
             $(".songList").append("<h2>" + "Songs" + "</h2>")
         data.forEach(function(song){
-            $(".songList").append(`<iframe width="51%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${song.id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
-            $(".songList").scroll();    
+            $(".songList").append(`<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${song.id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`) 
         })
-    });  
+    });
+    $(".songList").show();  
 });
 
 $( "#recommendationSearch").click(function() {
@@ -26,9 +27,9 @@ $( "#recommendationSearch").click(function() {
             $(".songList").empty();
             $(".songList").append("<h2>" + "Recommendations" + "</h2>")
         data.forEach(function(song){
-            $(".songList").append(`<iframe width="51%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${song.id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
-            $(".songList").scroll();
+            $(".songList").append(`<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${song.id}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`)
         })
-    });  
+    });
+    $(".songList").show();  
 });
 
